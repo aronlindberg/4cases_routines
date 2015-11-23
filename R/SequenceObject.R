e5f79d28172a27d64139c4be2d88649ad67c9161
Aron Lindberg <aron.lindberg@case.edu>
2015-11-23 11:57:38 -0500

    cosmetic edits

--- R/SequenceObject.R
+++ R/SequenceObject.R
@@ -3,13 +3,13 @@ SequenceObject <- function(data, startdate, stopdate, exclude = c(), type){
   
   # basic reformatting
   data <- read.table(data, sep = ",", header = TRUE)
-  data <- subset(data, select = c("pull_req_id", "action", "created_at"))
-  
+  data <- subset(data, select = c("pull_req_id", "user", "action", "created_at"))
+
   # exclude all activity types which are not wanted in the data
-  data <- subset(data,!action %in% exclude)
+  data <- subset(data,!(action %in% exclude))
   
   # select only data between startdate and stopdate
-  colnames(data) <- c("id", "event", "time")
+  colnames(data) <- c("id", "actor", "event", "time")
   data <- sqldf(paste0("SELECT * FROM data WHERE strftime('%Y-%m-%d', time,
                        'unixepoch', 'localtime') >= '",startdate,"' AND strftime('%Y-%m-%d', time,
                        'unixepoch', 'localtime') <= '",stopdate,"'"))
@@ -24,8 +24,7 @@ SequenceObject <- function(data, startdate, stopdate, exclude = c(), type){
     }
     data <- list.to.df(data.split)
     data <- t(data)
-    (data)
-    
+
     # Another type of output for TraMineR TSE format
   } else {
     data$end <- data$time
@@ -33,6 +32,7 @@ SequenceObject <- function(data, startdate, stopdate, exclude = c(), type){
     data$time <- match( data$time , unique( data$time ) )
     data$end <- match( data$end , unique( data$end ) )
     slmax <- max(data$time)
-    (data)
+    
   }
+  (arrange(data, id))
 }
