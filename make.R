8ba01d67db18fc524c680f6c711dead85c3337cb
Aron Lindberg <aron.lindberg@case.edu>
2016-03-25 19:37:11 -0400

    added RcolorBrewer

--- make.R
+++ make.R
@@ -2,7 +2,7 @@ setwd("/Users/aron/git/4cases_routines/")
 
 # Load packages
 required_packages <- c('TraMineR','cluster','stringr', 'network', 'sna', 'sqldf',
-                       'WeightedCluster', 'knitr', 'irr', 'stringr', 'plyr', 'igraph')
+                       'WeightedCluster', 'knitr', 'irr', 'stringr', 'plyr', 'igraph', 'RColorBrewer')
 new_packages <- required_packages[!(required_packages %in% installed.packages()[,'Package'])]
 if(length(new_packages)) install.packages(new_packages)
 lapply(required_packages, library, character.only = TRUE)
